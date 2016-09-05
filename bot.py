eb9a03f1076c98bac812329428f300648c8fd4b3
parthshukl <parthshukla@mail.com>
2016-09-05 19:21:22 +0530

    Added bot.py

---
+++ bot.py
@@ -0,0 +1,92 @@
+#Plz dont judge
+import pyautogui
+import time
+import subprocess
+from selenium 
+count=0
+while count<10:
+	driver=webdriver.Firefox();
+	driver.get("#formlinkgoeshere");
+	driver.find_element_by_xpath('//*[@id="1438049"]/div[2]/div[3]/div/a').click()
+	pyautogui.typewrite('Hello world!\n', 0)
+	pyautogui.typewrite(['enter'])
+	time.sleep(0.5)
+	pyautogui.typewrite('Hello world!\n', 0)
+	pyautogui.typewrite(['enter'])
+	time.sleep(0.5)
+	pyautogui.typewrite('spam@spam.com\n', 0)
+	pyautogui.typewrite(['enter'])
+	time.sleep(2)
+	pyautogui.typewrite(['s'])
+	pyautogui.typewrite(['enter'])
+	time.sleep(3)
+	pyautogui.typewrite(['s'])
+	pyautogui.typewrite(['enter'])
+	time.sleep(3)
+	pyautogui.typewrite(['a'])
+	time.sleep(3)
+	pyautogui.typewrite(['9','9','9','9','9','9'],0)
+	pyautogui.typewrite(['enter'])
+	time.sleep(3)
+	pyautogui.typewrite(['s'])
+	pyautogui.typewrite(['enter'])
+	time.sleep(2)
+	pyautogui.typewrite('Challenger University\n', 0)
+	pyautogui.typewrite(['enter'])
+	time.sleep(2)
+	pyautogui.typewrite(['Y'])
+	time.sleep(1)
+	pyautogui.typewrite(['Y'])
+	time.sleep(1)
+	pyautogui.typewrite(['Y'])
+	time.sleep(1)
+	pyautogui.typewrite(['enter'])
+	time.sleep(0.3)
+	pyautogui.typewrite(['enter'])
+	time.sleep(0.3)
+	pyautogui.typewrite(['enter'])
+	time.sleep(0.3)
+	pyautogui.typewrite(['enter'])
+	time.sleep(0.3)
+	pyautogui.typewrite(['Y'])
+	time.sleep(1)
+	pyautogui.typewrite('Challenges\n', 0)
+	pyautogui.typewrite(['enter'])
+	time.sleep(1)
+	pyautogui.typewrite('thisbot\n', 0)
+	pyautogui.typewrite(['enter'])
+	time.sleep(1)
+	pyautogui.typewrite(['A'])
+	time.sleep(0.5)
+	pyautogui.typewrite(['D'])
+	pyautogui.typewrite('pesca pescatarian\n', 0)
+	pyautogui.typewrite(['enter'])
+	time.sleep(1)
+	pyautogui.typewrite(['N'])
+	time.sleep(1)
+	pyautogui.typewrite(['enter'])
+	pyautogui.typewrite(['enter'])
+	time.sleep(10)
+	pyautogui.doubleClick(x=585, y=179)
+	pyautogui.typewrite(['enter'])
+	time.sleep(3)
+	pyautogui.typewrite(['enter'])
+	pyautogui.typewrite(['enter'])
+	pyautogui.typewrite(['enter'])
+	pyautogui.typewrite(['enter'])
+	pyautogui.typewrite(['enter'])
+	pyautogui.typewrite(['enter'])
+	pyautogui.typewrite(['enter'])
+	subprocess.call(['osascript', '-e', 'tell application "Firefox" to quit'])
+	count +=1
+
+#Kthenks
+
+
+
+
+
+
+
+
+
